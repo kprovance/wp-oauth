9fc07f7e8fc8780a73ee40aa18d1c372581ed3d4
Kevin Provance <kevin.provance@gmail.com>
2019-02-19 04:52:01 -0500

    TOTAL refactor.

--- uninstall.php
+++ uninstall.php
@@ -14,9 +14,11 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 // Delete all plugin settings ONLY if the user requested it.
 global $wpdb;
 
+// phpcs:disable WordPress.DB.DirectDatabaseQuery
 $delete_settings = $wpdb->get_var( "SELECT option_value FROM $wpdb->options WHERE option_name = 'wpoa_delete_settings_on_uninstall'" );
 
 if ( $delete_settings ) {
 	$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'wpoa_%';" );
 	$wpdb->query( "DELETE FROM $wpdb->usermeta WHERE meta_key LIKE 'wpoa_%';" );
 }
+// phpcs:enable WordPress.DB.DirectDatabaseQuery
