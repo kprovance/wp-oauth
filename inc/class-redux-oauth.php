71f66b6a35aaac6edf3ed7f7519cdc52032cd2ba
Kevin Provance <kevin.provance@gmail.com>
2019-02-21 17:39:27 -0500

    WPCS

--- inc/class-redux-oauth.php
+++ inc/class-redux-oauth.php
@@ -2,12 +2,12 @@
 /**
  * Redux oAuth Class
  *
- * @class Redux_OAuth
+ * @class   Redux_OAuth
  * @version 1.0.0
  * @package WP-OAuth
  */
 
-//defined( 'ABSPATH' ) || exit;
+defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'Redux_OAuth', false ) ) {
 	/**
@@ -62,39 +62,50 @@ if ( ! class_exists( 'Redux_OAuth', false ) ) {
 			if ( ! $this->config['client_enabled'] ) {
 				WPOA::$login->end_login( 'This third-party authentication provider has not been enabled. Please notify the admin or try again later.' );
 			} elseif ( ! $this->config['client_id'] || ! $this->config['client_secret'] ) {
+
 				// Do not proceed if id or secret is null.
 				WPOA::$login->end_login( 'This third-party authentication provider has not been configured with an API key/secret. Please notify the admin or try again later.' );
 			} elseif ( isset( $_GET['error_description'] ) ) {
+
 				// do not proceed if an error was detected.
 				WPOA::$login->end_login( sanitize_text_field( wp_unslash( $_GET['error_description'] ) ) ); // WPCS: CSRF ok.
 			} elseif ( isset( $_GET['error_message'] ) ) {
+
 				// do not proceed if an error was detected.
 				WPOA::$login->end_login( sanitize_text_field( wp_unslash( $_GET['error_message'] ) ) ); // WPCS: CSRF ok.
 			} elseif ( isset( $_GET['code'] ) ) { // WPCS: CSRF ok.
+
 				// post-auth phase, verify the state.
 				if ( isset( $_GET['state'] ) && $_GET['state'] === $_SESSION['WPOA']['STATE'] ) { // WPCS: CSRF ok.
+
 					// get an access token from the third party provider.
 					$this->get_oauth_token( sanitize_text_field( wp_unslash( $_GET['code'] ) ) ); // WPCS: CSRF ok.
+
 					// get the user's third-party identity and attempt to login/register a matching WordPress user account.
-					$login_callback = "WPOA_" . $this->config['provider'] . "_get_oauth_identity";
+					$login_callback = 'WPOA_' . $this->config['provider'] . '_get_oauth_identity';
+
 					if ( has_action( $login_callback ) ) {
 						$oauth_identity = apply_filters( $login_callback, $this );
 					} else {
 						$oauth_identity = $this->get_oauth_identity();
 					}
+
 					if ( ! isset( $oauth_identity['id'] ) ) {
 						WPOA::$login->end_login( 'Sorry, we couldn\'t log you in. User identity was not found. Please notify the admin or try again later.' );
 					}
-					WPOA::$login->login_user( $oauth_identity );
 
+					WPOA::$login->login_user( $oauth_identity );
 				} else {
+
 					// possible CSRF attack, end the login with a generic message to the user and a detailed message to the admin/logs in case of abuse:
 					// TODO: report detailed message to admin/logs here...
 					WPOA::$login->end_login( "Sorry, we couldn't log you in. Please notify the admin or try again later." );
 				}
 			} else {
+
 				// pre-auth, start the auth process.
 				if ( ( empty( $_SESSION['WPOA']['EXPIRES_AT'] ) ) || ( time() > $_SESSION['WPOA']['EXPIRES_AT'] ) ) {
+
 					// expired token; clear the state.
 					WPOA::$login->clear_state();
 				}
@@ -173,31 +184,33 @@ if ( ! class_exists( 'Redux_OAuth', false ) ) {
 		 * Function cURL.
 		 *
 		 * @param array  $params Params.
-		 * @param string $url URL.
-		 * @param bool   $post Is post.
+		 * @param string $url    URL.
+		 * @param bool   $post   Is post.
 		 *
 		 * @return bool|string
 		 */
 		public function curl( $params, $url, $post = false ) {
-			$curl       = curl_init();
+			$curl = curl_init();
 
 			if ( isset( $this->config['authorization_header'] ) && isset( $params['access_token'] ) ) {
 				$headr = array( 'Authorization: ' . $this->config['authorization_header'] . ' ' . $params['access_token'] );
-                curl_setopt( $curl, CURLOPT_HTTPHEADER, $headr );
+				curl_setopt( $curl, CURLOPT_HTTPHEADER, $headr );
 				unset( $params['access_token'] );
-            }
-			if ( is_array( $params ) && count( $params  ) ) {
-				$url_params = http_build_query( $params );
-				$url = $url . $url_params;
 			}
+
+			if ( is_array( $params ) && count( $params ) ) {
+				$url_params = http_build_query( $params );
+				$url        = $url . $url_params;
+			}
+
 			curl_setopt( $curl, CURLOPT_URL, $url );
 			curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
 
 			if ( $post ) {
 				curl_setopt( $curl, CURLOPT_POST, 1 );
 				curl_setopt( $curl, CURLOPT_POSTFIELDS, $params );
-				curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, ( $this->config['util_verify_ssl'] == 1 ? 1 : 0 ) );
-				curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, ( $this->config['util_verify_ssl'] == 1 ? 2 : 0 ) );
+				curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, ( '1' === $this->config['util_verify_ssl'] ? 1 : 0 ) );
+				curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, ( '1' === $this->config['util_verify_ssl'] ? 2 : 0 ) );
 			}
 
 			$result = curl_exec( $curl );
@@ -209,7 +222,7 @@ if ( ! class_exists( 'Redux_OAuth', false ) ) {
 		 * Steam.
 		 *
 		 * @param array  $params Params.
-		 * @param string $url URL.
+		 * @param string $url    URL.
 		 *
 		 * @return false|string
 		 */
@@ -290,21 +303,25 @@ if ( ! class_exists( 'Redux_OAuth', false ) ) {
 		/**
 		 * Get OAuth identity.
 		 *
- 		 * @return array|mixed|object
+		 * @return array|mixed|object
 		 */
 		public function get_oauth_identity() {
+
 			// here we exchange the access token for the user info...
 			// set the access token param.
 			$params = array();
+
 			if ( isset( $this->config['get_oauth_identity']['params'] ) ) {
 				$params = $this->config['get_oauth_identity']['params'];
 			}
+
 			$params['access_token'] = $_SESSION['WPOA']['ACCESS_TOKEN'];
+
 			$url = $this->config['url_user'];
-//			echo $url;
-//			print_r($params);
+
 			$result_obj = 'curl' === $this->config['http_util'] ? $this->curl( $params, $url ) : $this->stream( $params, $url );
 			$result_obj = json_decode( $result_obj, true );
+
 			// parse and return the user's oauth identity.
 			$oauth_identity             = $result_obj;
 			$oauth_identity['provider'] = $_SESSION['WPOA']['PROVIDER'];
@@ -313,4 +330,3 @@ if ( ! class_exists( 'Redux_OAuth', false ) ) {
 		}
 	}
 }
-
