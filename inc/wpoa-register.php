bd70e4f8d45028d13631c61f6bde4f7a58bc1e75
Kevin Provance <kevin.provance@gmail.com>
2019-03-12 18:14:17 -0400

    blogname in email subject

--- inc/wpoa-register.php
+++ inc/wpoa-register.php
@@ -107,7 +107,7 @@ if ( false === $update_username_result ) { // || false === $update_nickname_resu
 	if ( '1' === get_option( 'wpoa_email_notify_link' ) ) {
 		$message = $current_user->user_login . ' created an account using their ' . $_SESSION['WPOA']['PROVIDER'] . ' account.' . "\n\n" . 'Username: ' . $oauth_identity['name'] . "\n\n" . 'EMail: ' . $oauth_identity['email'];
 
-		$x = wp_mail( WPOA::$admin_email, '[Redux.io] New Account', $message );
+		$x = wp_mail( WPOA::$admin_email, '[' . get_option( 'blogname' )'] New Account', $message );
 	}
 
 	// Attempt to login the new user (this could be error prone).
